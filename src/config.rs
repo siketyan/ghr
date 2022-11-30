fa9ca066e4897beb33dff871cbddf1fbe0c5296b
Naoki Ikeguchi <me@s6n.jp>
2022-11-30 12:50:15 +0900

    feat: Load config from ghr.toml

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 use std::fs::read_to_string;
+use std::path::Path;
 
 use anyhow::Result;
 use serde::Deserialize;
+use tracing::warn;
 
 use crate::application::Applications;
 use crate::git::Config as GitConfig;
@@ -23,15 +25,30 @@ pub struct Config {
 
 impl Config {
     pub fn load_from(root: &Root) -> Result<Self> {
-        let path = root.path().join("config.toml");
+        Ok(match Self::load_from_path(root.path().join("ghr.toml"))? {
+            Some(c) => c,
+            None => {
+                warn!(
+                    "Using `config.toml` is deprecated since ghr v0.2.3 and \
+                    it won't be supported from v0.3. Move them to `ghr.toml` to migrate.",
+                );
 
-        Ok(match path.exists() {
-            true => toml::from_str(read_to_string(path)?.as_str())?,
-            _ => Self::default(),
+                Self::load_from_path(root.path().join("config.toml"))?.unwrap_or_default()
+            }
         })
     }
 
     pub fn load() -> Result<Self> {
         Self::load_from(&Root::find()?)
+    }
+
+    fn load_from_path<P>(path: P) -> Result<Option<Self>>
+    where
+        P: AsRef<Path>,
+    {
+        Ok(match path.as_ref().exists() {
+            true => Some(toml::from_str(read_to_string(path)?.as_str())?),
+            _ => None,
+        })
     }
 }
