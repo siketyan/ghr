a8d311c7a9f82af8272eeff93e6d485fa471fe6b
Naoki Ikeguchi <me@s6n.jp>
2023-01-16 20:50:32 +0900

    feat: Support listing repos without host or owner

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -6,7 +6,15 @@ use crate::repository::Repositories;
 use crate::root::Root;
 
 #[derive(Debug, Parser)]
-pub struct Cmd {}
+pub struct Cmd {
+    /// Lists repositories without their hosts.
+    #[clap(long)]
+    no_host: bool,
+
+    /// Lists repositories without their owners.
+    #[clap(long)]
+    no_owner: bool,
+}
 
 impl Cmd {
     pub fn run(self) -> Result<()> {
@@ -14,7 +22,7 @@ impl Cmd {
 
         Repositories::try_collect(&root)?
             .into_iter()
-            .map(|(path, _)| path.to_string())
+            .map(|(path, _)| path.to_string_with(!self.no_host, !self.no_owner))
             .sorted()
             .for_each(|path| println!("{}", path));
 
