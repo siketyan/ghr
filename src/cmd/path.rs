51d8040c9b044c36470efb49074f33f84ba1cbcf
Naoki Ikeguchi <me@s6n.jp>
2023-01-16 14:53:17 +0900

    feat!: Support resolving path from URL or patterns

--- src/cmd/path.rs
+++ src/cmd/path.rs
@@ -3,9 +3,10 @@ use std::path::PathBuf;
 use anyhow::{anyhow, Result};
 use clap::Parser;
 
-use crate::path::PartialPath;
+use crate::config::Config;
+use crate::path::{PartialPath, Path};
 use crate::root::Root;
-use crate::url::Host;
+use crate::url::{Host, Url};
 
 #[derive(Debug, Parser)]
 pub struct Cmd {
@@ -14,6 +15,8 @@ pub struct Cmd {
     #[clap(long)]
     host: Option<String>,
     /// Owner name of the repository.
+    /// Defaults to the default owner if it is configured.
+    #[clap(short, long)]
     owner: Option<String>,
     /// Repository name.
     repo: Option<String>,
@@ -22,17 +25,27 @@ pub struct Cmd {
 impl Cmd {
     pub fn run(self) -> Result<()> {
         let root = Root::find()?;
-        let path = PartialPath {
-            root: &root,
-            host: match self.owner.is_some() || self.repo.is_some() {
-                true => self.host.or_else(|| Some(Host::GitHub.to_string())),
-                _ => self.host,
-            },
-            owner: self.owner,
-            repo: self.repo,
+        let config = Config::load_from(&root)?;
+
+        let path = if let Some(repo) = self.repo.as_deref() {
+            let url = Url::from_str(
+                repo,
+                self.owner.as_deref().or(config.defaults.owner.as_deref()),
+            )?;
+
+            PathBuf::from(Path::resolve(&root, &url))
+        } else {
+            PathBuf::from(PartialPath {
+                root: &root,
+                host: match self.owner.is_some() || self.repo.is_some() {
+                    true => self.host.or_else(|| Some(Host::GitHub.to_string())),
+                    _ => self.host,
+                },
+                owner: self.owner,
+                repo: None,
+            })
         };
 
-        let path = PathBuf::from(path);
         if !path.exists() || !path.is_dir() {
             return Err(anyhow!(
                 "The path does not exist or is not a directory. Did you cloned the repository?"
