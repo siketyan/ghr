f29ad31dc7dba806cebc918552382babcd74ae44
Yuichiro Luke Smith <contact@yu-smith.com>
2023-04-07 09:43:09 +0900

    feat: Add --short option to `profile list` for completion uses

--- src/cmd/profile/list.rs
+++ src/cmd/profile/list.rs
@@ -7,32 +7,40 @@ use crate::config::Config;
 const INHERIT: &str = "(inherit)";
 
 #[derive(Debug, Parser)]
-pub struct Cmd;
+pub struct Cmd {
+    /// Shows only the name of the profiles
+    #[clap(short, long)]
+    short: bool,
+}
 
 impl Cmd {
     pub fn run(self) -> Result<()> {
         let config = Config::load()?;
 
         config.profiles.iter().for_each(|(name, profile)| {
-            println!(
-                "   {} - {}: {} {}",
-                style("OK").cyan(),
-                style(name).bold(),
-                profile
-                    .configs
-                    .get("user.name")
-                    .map(|name| name.as_str())
-                    .unwrap_or(INHERIT),
-                style(&format!(
-                    "<{}>",
+            if self.short {
+                println!("{}", name)
+            } else {
+                println!(
+                    "   {} - {}: {} {}",
+                    style("OK").cyan(),
+                    style(name).bold(),
                     profile
                         .configs
-                        .get("user.email")
-                        .map(|email| email.as_str())
+                        .get("user.name")
+                        .map(|name| name.as_str())
                         .unwrap_or(INHERIT),
-                ))
-                .dim(),
-            );
+                    style(&format!(
+                        "<{}>",
+                        profile
+                            .configs
+                            .get("user.email")
+                            .map(|email| email.as_str())
+                            .unwrap_or(INHERIT),
+                    ))
+                    .dim(),
+                );
+            }
         });
 
         Ok(())
